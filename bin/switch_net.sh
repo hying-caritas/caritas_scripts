2323e35e133a0cf50747711e3161c85fb16e378d
Huang Ying <huang.ying.caritas@gmail.com>
2013-02-16 11:33:17 +0800

    Enhanced switch_net.sh

--- bin/switch_net.sh
+++ bin/switch_net.sh
@@ -2,7 +2,14 @@
 
 source caritas_functions.sh
 
-INTERNAL_DOMAINS=
+WORK_HTTP_PROXY_HOST=
+WORK_HTTP_PROXY_PORT=
+HOME_HTTP_PROXY_HOST=
+HOME_HTTP_PROXY_PORT=
+WORK_HOSTS=
+HOME_HOSTS=
+WORK_INTERNAL_DOMAINS=
+HOME_INTERNAL_DOMAINs=
 
 load_config
 
@@ -24,14 +31,55 @@ if [ $tnet != "home" -a $tnet != "work" ]; then
 	exit -1
 fi
 
-if [ "$tnet" == "work" ]; then
-	sudo cp /etc/apt/apt.conf.avail/99proxy /etc/apt/apt.conf.d
-	# gconftool-2 -s -t string /system/proxy/mode auto
+setup_proxy()
+{
+	local proxy_host="$1"
+	local proxy_port="$2"
+	local internal_domains="$3"
+	[ $# -ne 3 ] && die_invalid
+
+	if [ -z "$proxy_host" ]; then
+		sudo rm -f /etc/apt/apt.conf.avail/99proxy
+		gsettings set org.gnome.system.proxy mode none
+		gsettings set org.gnome.system.proxy ignore-hosts "['localhost', '127.0.0.0/8']"
+		return
+	fi
+	echo "Acquire::http::Proxy \"http://$proxy_host/$proxy_port\";
+Acquire::ftp::Proxy \"http://$proxy_host/$proxy_port\";" |
+		sudo_outf /etc/apt/apt.conf.avail/99proxy
 	gsettings set org.gnome.system.proxy mode manual
-	gsettings set org.gnome.system.proxy ignore-hosts "['localhost', '127.0.0.0/8', $INTERNAL_DOMAINS]"
+	for proto in http https ftp; do
+		gsettings set org.gnome.system.proxy.$proto host "$proxy_host"
+		gsettings set org.gnome.system.proxy.$proto port "$proxy_port"
+	done
+	if [ -n "$internal_domains" ]; then
+		gsettings set org.gnome.system.proxy ignore-hosts "['localhost', '127.0.0.0/8', $internal_domains]"
+	else
+		gsettings set org.gnome.system.proxy ignore-hosts "['localhost', '127.0.0.0/8']"
+	fi
+}
+
+HOSTS_PREFIX="# ==== CARITAS SWITCH_NET BEGIN ===="
+HOSTS_POSTFIX="# ==== CARITAS SWITCH_NET END ===="
+
+setup_hosts()
+{
+	local hosts="$1"
+	[ $# -ne 1 ] && die_invalid
+
+	sudo sed -ie "/$HOSTS_PREFIX/,/$HOSTS_POSTFIX/d" /etc/hosts
+	if [ -z "$hosts" ]; then
+		return
+	fi
+	echo "$HOSTS_PREFIX
+$hosts
+$HOSTS_POSTFIX" | sudo_outf -a /etc/hosts
+}
+
+if [ "$tnet" == "work" ]; then
+	setup_proxy "$WORK_HTTP_PROXY_HOST" "$WORK_HTTP_PROXY_PORT" "$WORK_INTERNAL_DOMAINS"
+	setup_hosts "$WORK_HOSTS"
 else
-	sudo rm -f /etc/apt/apt.conf.d/99proxy
-	# gconftool-2 -s -t string /system/proxy/mode none
-	gsettings set org.gnome.system.proxy mode none
-	gsettings set org.gnome.system.proxy ignore-hosts "['localhost', '127.0.0.0/8']"
+	setup_proxy "$HOME_HTTP_PROXY_HOST" "$HOME_HTTP_PROXY_PORT" "$HOME_INTERNAL_DOMAINS"
+	setup_hosts "$HOME_HOSTS"
 fi
