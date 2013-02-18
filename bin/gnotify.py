a6397d4ecc9501f4850e7f8f3e329de3564aa92d
Huang Ying <huang.ying.caritas@gmail.com>
2013-02-18 10:02:29 +0800

    Add timeout/application support to gnotify.py

--- bin/gnotify.py
+++ bin/gnotify.py
@@ -2,17 +2,40 @@
 
 import pynotify
 import sys
+import os.path
+from optparse import OptionParser
+import time
 
-def notify(title="Notify", message=""):
+def notify(timeout, title, message):
     n = pynotify.Notification(title, message)
+    n.set_timeout(timeout)
     n.show()
+    time.sleep(timeout / 1000)
+    n.close()
 
+def main(argv):
+    cmd = os.path.basename(argv[0])
+    parser = OptionParser("Usage: %s [options] <title> [msg...]" % cmd)
+    parser.add_option("-a", "--application", dest="app", help="application name")
+    parser.add_option("-t", "--timeout", dest="timeout", help="timeout in miliseconds")
+    (options, args) = parser.parse_args()
+    if len(args) == 0:
+        title =  'Notification'
+        msg = ''
+    else:
+        title = args[0]
+        msg= " ".join(args[1:])
+    if options.app:
+        app = options.app
+    else:
+        app = cmd
+    if options.timeout:
+        timeout = int(options.timeout)
+    else:
+        timeout = 0
+
+    pynotify.init(app)
+    notify(timeout, title, msg)
 
 if __name__ == '__main__':
-    pynotify.init("cli notify")
-    if len(sys.argv) > 1:
-        title = sys.argv[1]
-        msg = " ".join(sys.argv[2:])
-        notify(title, msg)
-    else:
-        notify()
+    main(sys.argv)
