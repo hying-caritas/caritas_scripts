765906b4b21d2baa2aa24bdc7e4f1b62339392e0
Huang Ying <huang.ying.caritas@gmail.com>
2014-10-01 08:15:53 +0800

    bin/git_is_clean.sh: add non-quiet mode

--- bin/git_is_clean.sh
+++ bin/git_is_clean.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-! [[ $(git status --porcelain) ]]
+status=$(git status --porcelain)
+
+if [[ $1 == -q ]]; then
+	if [[ $status ]]; then
+		exit 1
+	else
+		exit 0
+	fi
+fi
+
+if [[ $status ]]; then
+	echo "$status"
+else
+	echo "git is clean!"
+fi
