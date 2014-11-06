9afbdc500cb2965e51fad2269bc3411137d0e0a3
Huang Ying <ying.huang@intel.com>
2014-11-06 12:28:42 +0800

    Add export_x_info.sh to export X/dbus information for cron tasks to use

---
+++ bin/export_x_info.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Export the dbus session address on startup so it can be used by cron
+# Based on: http://jason.the-graham.com/2011/01/16/gnome_keyring_with_msmtp_imapfilter_offlineimap/
+
+: > $HOME/.Xdbus
+chmod 600 $HOME/.Xdbus
+
+export_var()
+{
+	var=$1
+	echo export $var=${!var} >> $HOME/.Xdbus
+}
+
+export_var DISPLAY
+export_var DBUS_SESSION_BUS_ADDRESS
