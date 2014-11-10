c23ba21d8e09c27b9612031e4bb6822e87116c74
Huang Ying <ying.huang@intel.com>
2014-11-10 13:14:44 +0800

    Add deb-edit.sh
    Edit control file and re-package.

---
+++ bin/deb-edit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+program=$(basename "$0")
+deb=$1
+ndeb=${deb/%.deb/_modified.deb}
+tmp=(mktemp "$pgoram")
+
+trap "rm -rf $tmp" EXIT
+
+dpkg-deb -R "$deb" "$tmp"
+vim $tmp/DEBIAN/control
+dpkg-deb -b "$tmp" "$ndeb"
