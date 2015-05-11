ad4870cf3583e5fed834efc41740d5b5b5dc1dfe
Huang Ying <ying.huang@intel.com>
2015-05-11 09:42:02 +0800

    bin/git_force_pull.sh: Make it worked for any current branch

--- bin/git_force_pull.sh
+++ bin/git_force_pull.sh
@@ -4,9 +4,15 @@ if ! git branch | grep -qw tmp; then
 	git branch tmp
 fi
 
+git_current_branch()
+{
+	git branch | grep '^\* ' | cut -d ' ' -f 2-
+}
+
+current_branch=$(git_current_branch)
 git remote update
 git reset --hard
 git checkout tmp
-git branch -D master
-git branch master origin/master
-git checkout master
+git branch -D $current_branch
+git branch $current_branch origin/master
+git checkout $current_branch
